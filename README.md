8d73de83cb4611b67eebfd609157bd3ae7dc152c
Suraf <surafbrov@outlook.com>
2019-12-18 18:08:55 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# RemoveSelectionCircle
+
+This addon removes the selection circle from the target. This helps to take screenshots better for Wowpedia.
+
+# Credits
+* Ketho, for the [original code](https://www.mmo-champion.com/threads/2033138-Self-targeting-floor-circle?p=41527606&viewfull=1#post41527606)
+* Suraf, modified to apply to NPCs
---
+++ RemoveSelectionCircle/core.lua
@@ -0,0 +1,14 @@
+-- from https://www.mmo-champion.com/threads/2033138-Self-targeting-floor-circle?p=41527606&viewfull=1#post41527606
+-- credits to Ketho (thanks)
+-- modified to apply to the target (remove the selection circle from the NPCs) to take better screenshots for Wowpedia
+
+local f = CreateFrame("Frame")
+
+function f:OnEvent(event)
+	if not IsInGroup() then -- not in raid/battleground
+		SetCVar("ObjectSelectionCircle", UnitIsUnit("target", "target") and 0 or 1)
+	end
+end
+
+f:RegisterEvent("PLAYER_TARGET_CHANGED")
+f:SetScript("OnEvent", f.OnEvent)
